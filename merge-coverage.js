5c44eb6008f6314d26be2dc59880f9c84f3f7e99
terniea <129784004+terniea@users.noreply.github.com>
2024-02-24 07:55:58 -0500

    Update merge-coverage.js

--- merge-coverage.js
+++ merge-coverage.js
@@ -14,7 +14,7 @@ const codecovTargets = require('../coverage-targets');
 // Temporarily commented out as we can't rely on the commented yaml file
 // Can be restored when the codecov checks are restored. In the meantime
 // the important parts of the yaml file are copied below in normal js object
-// format.
+// formats.
 // const codecovConfig = yaml.load(fs.readFileSync('codecov.yml', 'utf8'));
 
 const codecovConfig = {
