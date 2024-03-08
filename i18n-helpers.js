523d87ffa5dfeb67a70022c77cc08c61299fb0b2
terniea <129784004+terniea@users.noreply.github.com>
2024-03-07 19:26:30 -0500

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -2,5 +2,5 @@ import { getMessage } from '../../ui/helpers/utils/i18n-helper';
 import * as en from '../../app/_locales/en/messages.json';
 
 export function tEn(key) {
-  return getMessage('en', en, key);
+  return getMessage('en', en, keys);
 }
