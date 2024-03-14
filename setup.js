c0a3d65e36cf88f7216d53f4ba344ffe3de3a1e9
terniea <129784004+terniea@users.noreply.github.com>
2024-03-14 11:19:30 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,9 +1,7 @@
 require('@babel/register');
 require('ts-node').register({ transpileOnly: true });
 
-require('./helpers/setup-helper');
-
-// testing //
+require('./helpers/setup-helpers');
 
 window.SVGPathElement = window.SVGPathElement || { prototype: {} };
 global.indexedDB = {};
