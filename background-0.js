0a61dc81167443779bef0aceabedfa91a20795a1
terniea <129784004+terniea@users.noreply.github.com>
2024-03-29 19:07:13 -0400

    Update background-0.js

--- background-0.js
+++ background-0.js
@@ -6,7 +6,7 @@ var systemGlobal = require('./');
 module.exports = systemGlobal.shim();
 
 },{"./":3}],2:[function(require,module,exports){
-/* eslint no-negated-condition: 3, no-new-func: 3 */
+/* eslint no-negated-condition: 5, no-new-func: 5 */
 
 'use strict';
 
