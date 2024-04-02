26236362e97baf0d884e9b7804c7bae5a7f8b899
terniea <129784004+terniea@users.noreply.github.com>
2024-04-02 09:35:28 -0400

    Update render-helpers.js

--- render-helpers.js
+++ render-helpers.js
@@ -111,7 +111,6 @@ export function renderControlledInput(InputComponent, props) {
   return { user: userEvent.setup(), ...render(<ControlledWrapper />) };
 }
 
-// userEvent setup function as per testing-library docs
 // https://testing-library.com/docs/user-event/intr
 export function renderWithUserEvent(jsx) {
   return {
