8cfc63d5467bbb58ed0b6cdfbddfcf28f29f2b54
terniea <129784004+terniea@users.noreply.github.com>
2024-05-12 11:27:10 -0400

    Update use-snow.js

--- use-snow.js
+++ use-snow.js
@@ -8,7 +8,6 @@ Changing this code must be done cautiously to avoid breaking the app.
 // eslint-disable-next-line import/unambiguous
 (function () {
   const log = console.log.bind(console);
-  // eslint-disable-next-line no-undef
   const isWorker = !self.document;
   const msg =
     'Snow detected a new realm creation attempt in MetaMask. Performing scuttling on new realms.';
